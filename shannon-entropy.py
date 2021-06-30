be34e956d9bca3e840f3ed3596e08a21520e1efa
David M. Palmer <dmopalmer@gmail.com>
2021-06-29 22:54:07 -0600

    Faster entropy and random sequence comparison.

--- shannon-entropy.py
+++ shannon-entropy.py
@@ -6,6 +6,7 @@ import collections
 from collections import Counter
 
 seq = "tatgagtcatcacataaaagctaatgtagtaaagcatccataccaattgatttaaaattaaactaatccgttgataaaacaaaaaatcaagagccttgagtcactccagactgagaagattgactcaagaacaaattttatttttattttattagaggctccattggaaaaataagacctaaacattaatcgagaagtgatgggaacgatggaacctgtaaatacataagatgttactgaaaacaaatcttaatgatttattgggaggatagcgaaatgaaccagaagacaatcgatttattttattatgagagatcatgggttacctctacaaataaaataactattgaaagactaaatatgcaagaggaaatattcgcccgcgaagattttttttatattctttttgattgctaaatttgatcaatctgatatcctaattcgaatatataaaaaaatttgttacaaccttataacaaataacaaaaacactattattgaaaatcaaaaaataaaatagaaaaaattctctataattataattaatgtgtataactagaataattttttctatttctatctataactattagatctagaactagtagaactctaaaatagaatatagattctaatttatatatattagatacaaatttatattctactaatattctattctacctaatatcctattctaataatctaagattctaatactaataaatagatcgaataagtaagaaataaattaaaataaatagatttaactaaattaagtgaaatctcaaagaatacgatgatttaatatattattttattcgtaaaagacatggatatttttttttaatcatttcattcgcgaggagctggatgagaagaaattctcatgtccggttctgtagtagagatggaattgagaaataaccatcaactataaccccaaaagaacccgattccgtaaacaacatagaggaagaatgaaaggaatagcttttcgaggaaatcgtatttgttttggaagatatgctcttcaagcacttgaatccgcttggattacatctaga"
+randomseq = "".join(np.random.choice(["a","t","c","g"], size=len(seq)))
 #from Euphorbia iharanae genome, arbitrary choice
 
 seq_list = list(seq)
@@ -15,13 +16,31 @@ def Shannon_entropy_func(sequence, blocksize):
     N=len(sequence) -blocksize +1
     Block=[sequence[i:i +blocksize] for i in range(N)]
     entropy = sum([math.log(float(N)/Block.count(b)) for b in Block])/N
-    print(entropy)
+    return (entropy)
 
-# maybe this should be return(entropy)
+def Shannon_entropy_dmp(sequence, blocksize):
+    N = len(sequence) - blocksize + 1
+    counter = Counter([sequence[i:i+blocksize] for i in range(N)])
+    counts = np.array(list(counter.values()))
 
-for blocksize in range(500):
-    Shannon_entropy_func(seq_list, blocksize)
-    blocksize += 1
+    # The following normalizations are to match Shannon_entropy_func
+    # and are not necessarily correct
+    normalization = -1/N
+    normalization_add = np.log(N)
+
+    entropy = np.sum(counts * np.log(counts)) * normalization + normalization_add
+    return entropy
+
+
+for blocksize in range(1, 20):
+    # Using seq instead of seq_list lets you treat the subsequences as
+    # strings instead of lists, which is faster
+    slowvalue = Shannon_entropy_func(seq, blocksize)
+    dmpvalue = Shannon_entropy_dmp(seq, blocksize)
+    randvalue = Shannon_entropy_dmp(randomseq, blocksize)
+    print(f"{blocksize:10d} {slowvalue:8.3f}  {dmpvalue:8.3f}     {randvalue:8.3f}")
+
+    #    blocksize += 1
 
 #goes through every block length 1 - 500 (somewhat arbitrary max
 #limit, need to look into where is the proper place to stop) and
