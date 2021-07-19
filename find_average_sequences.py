65a67e12177cd34e6f62a69fd68cce8315c4bb6f
lucasblakeslee <lqblakeslee@gmail.com>
2021-07-19 14:47:39 +0000

    epsilon test genomes

--- find_average_sequences.py
+++ find_average_sequences.py
@@ -1,13 +1,16 @@
 import os
 from pathlib import Path
 
-def average_files(directory_path):
+def main():
     db_directory_names = list(Path(".").glob("*_databases"))
+    for db_directory_name in db_directory_names:
+        average_files(db_directory_name)
 
-    files = os.listdir(directory_path)
+def average_files(directory_name):
+    files = os.listdir(directory_name)
     sequences = {}
     for file_name in files:
-        with open(directory_path + "/" + file_name) as file_data:
+        with open(os.path.join(directory_name, file_name)) as file_data:
             for line in file_data.readlines():
                 sequence = line[:line.find(" ")]
                 occurences = int(line[line.find(" ")+1:])
@@ -15,11 +18,12 @@ def average_files(directory_path):
                     sequences[sequence] = (sequences[sequence] + occurences)/2
                 else:
                     sequences[sequence] = occurences
-    
-    with open("epsilonproteobacteria_averages.db_txt", "w+") as f:
+
+    with open(f"{directory_name}_averages.db_txt", "w+") as f:
         output = ""
         for key, value in sequences.items():
             output = output + f"{key} {value}\n"
         f.write(output[:-1])
 
-average_files('epsilonproteobacteria_databases')
+if __name__ == "__main__":
+    main()
