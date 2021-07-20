d2f3f920b99750b8b0332f1ee7d318801855315c
lucasblakeslee <lqblakeslee@gmail.com>
2021-07-20 16:10:26 +0000

    added method for using nucleotide BLAST

---
+++ BLAST-experiment.py
@@ -0,0 +1,18 @@
+from Bio.Blast import NCBIWWW
+from Bio import SeqIO
+
+def main():
+    record = SeqIO.read("test_genomes/gamma_test_genomes/amphritea-japonica.fasta", format = "fasta")
+    result_handle = blast_search(record)
+    blast_result = open("my_blast.xml", "w+")
+    blast_result.write(result_handle.read())
+    blast_result.close()
+    result_handle.close()
+
+def blast_search(SeqIOrecord):
+    result_handle = NCBIWWW.qblast("blastn", "nt", SeqIOrecord.seq[1:100])
+    return result_handle
+
+if __name__ == "__main__":
+    main()
+
